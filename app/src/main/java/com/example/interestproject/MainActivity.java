0891a80ba963b0fba25dc5414fa039a868f6c510
ThomasObrecht13 <thomasobrecht12@gmail.com>
2021-12-30 17:38:18 +0100

    icon nav googleSignIn

--- app/src/main/java/com/example/interestproject/MainActivity.java
+++ app/src/main/java/com/example/interestproject/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.interestproject;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    MessageFragment messageFragment = new MessageFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setSelectedItemId(R.id.homeFragment);
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(R.id.nav_fragment, homeFragment)
+                .commit();
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener = new
+            NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.homeFragment:
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .replace(R.id.nav_fragment, homeFragment)
+                                    .commit();
+                            return true;
+                        case R.id.messageFragment:
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .replace(R.id.nav_fragment, messageFragment)
+                                    .commit();
+                            return true;
+                        case R.id.searchFragment:
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .replace(R.id.nav_fragment, searchFragment)
+                                    .commit();
+                            return true;
+                        case R.id.profileFragment:
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .replace(R.id.nav_fragment, profileFragment)
+                                    .commit();
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
