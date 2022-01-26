91a4eed40a491722320769a20ff95d05c61a6108
ThomasObrecht13 <thomasobrecht12@gmail.com>
2022-01-26 11:29:43 +0100

    Show message

--- app/src/main/java/com/example/interestproject/ChatActivity.java
+++ app/src/main/java/com/example/interestproject/ChatActivity.java
@@ -4,21 +4,34 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.interestproject.adapter.ChatAdapter;
+import com.example.interestproject.model.Chat;
 import com.example.interestproject.model.User;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -28,6 +41,15 @@ public class ChatActivity extends AppCompatActivity {
     CircleImageView profilPicture;
     Button backButton;
     DatabaseReference reference;
+    FirebaseUser firebaseUser;
+
+    ImageButton btnToSend;
+    EditText messageToSend;
+
+
+    ChatAdapter chatAdapter;
+    List<Chat> mChat;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +68,36 @@ public class ChatActivity extends AppCompatActivity {
         username = findViewById(R.id.username_chat_toolbar);
         profilPicture = findViewById(R.id.profil_picture_chat_toolbar);
 
+        btnToSend = findViewById(R.id.send_message_btn);
+        messageToSend = findViewById(R.id.message_to_send);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        recyclerView = findViewById(R.id.recycler_view_chat);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        //recupère les infos de l'utilisateur
         Intent intent = getIntent();
         String userId = intent.getStringExtra("userId");
 
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+        btnToSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("message", String.valueOf(messageToSend.getText()));
 
-
+                String msg = messageToSend.getText().toString();
+                if(!msg.isEmpty()){
+                    sendMessage(firebaseUser.getUid(),userId,msg);
+                }else{
+                    Toast.makeText(getApplicationContext(),"Vous ne pouvez pas envoyer un message vide",Toast.LENGTH_SHORT).show();
+                }
+                messageToSend.setText("");
+            }
+        });
 
         reference.addValueEventListener(new ValueEventListener() {
         @Override
@@ -69,6 +115,7 @@ public class ChatActivity extends AppCompatActivity {
                             .load(user.getImageURL())
                             .into(profilPicture);
                 }
+                readMessage(firebaseUser.getUid(), userId, user.getImageURL());
             }
 
         @Override
@@ -76,7 +123,45 @@ public class ChatActivity extends AppCompatActivity {
 
         }
     });
+    }
 
+    private void sendMessage(String sender, String receiver, String message){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String,Object> hashMap = new HashMap<>();
+        hashMap.put("sender",sender);
+        hashMap.put("receiver",receiver);
+        hashMap.put("message",message);
+
+        reference.child("Chats").push().setValue(hashMap);
+
+    }
+
+    private void readMessage(String myid, String userid, String imageURL){
+        mChat = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mChat.clear();
+                for ( DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Chat chat = snapshot.getValue(Chat.class);
+                    if(chat.getReceiver().equals(myid) && chat.getSender().equals(userid) ||
+                            chat.getReceiver().equals(userid) && chat.getSender().equals(myid)){
+                        mChat.add(chat);
+                    }
+
+                    chatAdapter = new ChatAdapter(ChatActivity.this, mChat, imageURL);
+                    recyclerView.setAdapter(chatAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/interestproject/adapter/ChatAdapter.java
@@ -0,0 +1,105 @@
+package com.example.interestproject.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.interestproject.R;
+import com.example.interestproject.model.Chat;
+import com.example.interestproject.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder>{
+    public static final int MSG_TYPE_LEFT = 0;
+    public static final int MSG_TYPE_RIGHT = 1;
+
+
+    private Context mContext;
+    private List<Chat> mChat;
+    private String imageURL;
+
+    FirebaseUser firebaseUser;
+
+
+    public ChatAdapter(android.content.Context mContext, List<Chat> mChat, String imageURL){
+        this.mContext = mContext;
+        this.mChat = mChat;
+        this.imageURL = imageURL;
+    }
+
+    @NonNull
+    @Override
+    public ChatAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(viewType == MSG_TYPE_RIGHT){
+            View view = LayoutInflater.from(mContext).inflate(R.layout.chat_item_right, parent, false);
+            return new ChatAdapter.ViewHolder(view);
+        }else{
+            View view = LayoutInflater.from(mContext).inflate(R.layout.chat_item_left, parent, false);
+            return new ChatAdapter.ViewHolder(view);
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatAdapter.ViewHolder holder, int position) {
+        Chat chat = mChat.get(position);
+
+        holder.show_message.setText(chat.getMessage());
+
+        if(imageURL.equals("default")){
+            Glide.with(mContext)
+                    .load("https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg")
+                    .into(holder.profilePictureInMessage);
+        }else{
+            Glide.with(mContext)
+                    .load(imageURL)
+                    .into(holder.profilePictureInMessage);
+
+        }
+
+    }
+
+
+    public Chat getItem(int position) {
+        return mChat.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mChat.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView show_message;
+        public CircleImageView profilePictureInMessage;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            show_message = itemView.findViewById(R.id.show_message);
+            profilePictureInMessage = itemView.findViewById(R.id.profil_picture_chat_in_message);
+
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(mChat.get(position).getSender().equals(firebaseUser.getUid())) {
+            return MSG_TYPE_RIGHT;
+        }else{
+            return MSG_TYPE_LEFT;
+        }
+    }
+}
